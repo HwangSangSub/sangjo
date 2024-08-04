dbe845ee99ca5eb62e080359d87dc07a1d05fbb8
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2024-08-04 15:23:08 +0900

    회원 파트 : 주소VO 추가
    회원 정보에는 주소정보또한 필요하다.
    먼저 회원 정보를 통한 로그인 회원가입 등
    기본적인 기능을 완료한뒤에 주소 정보를 덛붙일것이다.

---
+++ sangjo/src/main/java/sangjo/vo/AddressVO.java
@@ -0,0 +1,13 @@
+package sangjo.vo;
+
+import lombok.Data;
+
+@Data
+public class AddressVO {
+	int addressNo;
+	String memberId;
+	int addressCode;
+	String addressMAin;
+	String addressDetail;
+	int idDefault;
+}
