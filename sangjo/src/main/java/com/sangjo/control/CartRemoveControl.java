dcfe7567dcc8537b33fc47025b2d6ec1d0559e60
admin <admin@YD02-13>
2024-08-06 11:39:35 +0900

    08-06

--- sangjo/src/main/java/com/sangjo/control/CartRemoveControl.java
+++ sangjo/src/main/java/com/sangjo/control/CartRemoveControl.java
@@ -10,19 +10,17 @@ import com.sangjo.service.CartListServiceImpl;
 
 public class CartRemoveControl implements Control {
 
-    private CartListService cartListService = new CartListServiceImpl();
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 요청 파라미터에서 장바구니 번호를 가져옴
+		String cartNo = req.getParameter("cartNo");
 
-    @Override
-    public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 요청 파라미터에서 장바구니 번호를 가져옴
-        String cartNo = req.getParameter("cartNo");
-        
-        if (cartNo != null) {
+		CartListService cls = new CartListServiceImpl();
+		if (cartNo != null) {
             // 서비스 클래스의 메서드를 호출하여 장바구니 항목을 삭제
-            cartListService.removeCartLIst(cartNo);
+			cls.removeCartLIst(cartNo);
         }
 
-        // 성공적으로 삭제되었다고 가정하고, 리다이렉션 등의 처리를 할 수 있음
-        resp.sendRedirect("cartList.do"); // 장바구니 리스트 페이지로 리다이렉트
-    }
+		req.getRequestDispatcher("sangjo/cartList.tiles").forward(req, resp);
+}
 }
