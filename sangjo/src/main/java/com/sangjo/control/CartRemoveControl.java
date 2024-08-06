c25f167cf3772dfe99392d428cbff1aec5577e51
황상섭 <tkdtjq2177@gachita.kr>
2024-08-06 10:30:59 +0900

    마스터 병합 및 장바구니 삭제 컨트롤 생성

---
+++ sangjo/src/main/java/com/sangjo/control/CartRemoveControl.java
@@ -0,0 +1,19 @@
+package com.sangjo.control;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sangjo.common.Control;
+
+public class CartRemoveControl implements Control {
+
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}// end exec
+
+}// end class
