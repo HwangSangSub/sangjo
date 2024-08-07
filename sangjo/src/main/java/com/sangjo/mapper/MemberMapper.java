52bd7e45fa12c5f8704d67f3e9502b1742d97ca6
황상섭 <tkdtjq2177@gachita.kr>
2024-08-07 10:12:25 +0900

    1

--- sangjo/src/main/java/com/sangjo/mapper/MemberMapper.java
+++ sangjo/src/main/java/com/sangjo/mapper/MemberMapper.java
@@ -8,32 +8,35 @@ public interface MemberMapper {
 
 	/**
 	 * 아이디 값을 통해 회원 객체를 반환한다.
+	 * 
 	 * @param memberId
 	 * @return 회원 객체
 	 */
 	MemberVO selectMemberById(String memberId);
-	
+
 	/**
-	 * 아이디와 비번을 통해 회원객체를 반환한다.
-	 * 현제 사용 X
+	 * 아이디와 비번을 통해 회원객체를 반환한다. 현제 사용 X
+	 * 
 	 * @param memberId
 	 * @param memberPw
 	 * @return 회원 객체
 	 */
 	@Deprecated
-	MemberVO selectMemberByIdPw(@Param("id") String memberId,@Param("pw") String memberPw);
+	MemberVO selectMemberByIdPw(@Param("id") String memberId, @Param("pw") String memberPw);
 
 	/**
 	 * 회원 정보를 서버에 넣는다.
+	 * 
 	 * @param memberVO
 	 * @return 추가된 숫자
 	 */
 	int insertMember(MemberVO memberVO);
-	
+
 	int updateMember(MemberVO member);
 
 	/**
 	 * 이메일을 통해 회원객체를 반환한다.
+	 * 
 	 * @param memberEmail
 	 * @return 회원 객체
 	 */
@@ -41,17 +44,17 @@ public interface MemberMapper {
 
 	/**
 	 * 아이디 값을 통해 login_date를 수정한다.
+	 * 
 	 * @param memberId
 	 */
 	int updateLoginDateByMemberId(String memberId);
 
 	/**
 	 * 비밀번호 수정
+	 * 
 	 * @param newMemberPw
-	 * @param memberEmail 
+	 * @param memberEmail
 	 * @return 변경 개수
 	 */
 	int updateMemberPw(@Param("pw") String newMemberPw, @Param("email") String memberEmail);
-
->>>>>>> branch 'master' of https://github.com/HwangSangSub/sangjo.git
 }
