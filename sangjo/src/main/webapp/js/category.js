b2915148d5381a74776169cdb2c95a0d1bead38e
Soomin Kwon <tnals686@cu.ac.kr>
2024-08-12 17:27:33 +0900

    0812

--- sangjo/src/main/webapp/js/category.js
+++ sangjo/src/main/webapp/js/category.js
@@ -5,45 +5,30 @@
 const csvc = {
 	categorySelectList(categoryName, loadCallback) {
 		const xhtp = new XMLHttpRequest();
-		xhtp.open('get', 'categoryList.do?categoryName=' + categoryName);
+		xhtp.open('get', 'category.do?categoryName=' + categoryName);
+		xhtp.send();
+		xhtp.onload = loadCallback;
+	},
+	productList(cdName, loadCallback) {
+		const xhtp = new XMLHttpRequest();
+		xhtp.open('get', 'productList.do?cdName=' + cdName);
 		xhtp.send();
 		xhtp.onload = loadCallback;
 	}
 }
 
+const categorySelectList = document.querySelectorAll('#categorySelectList');
 
-const fas = document.querySelectorAll('.fas');
-
-/*for (let i = 0; i < card.length; i++) {
-	card[i].addEventListener('click', function(e) {
-		let cdName = e.target.innerHTML;
-		console.log(e.target);
-		console.log(cdName);
-		//console.log(this);
-		svc.productList(cdName, function() {
-			// 카테고리 선택 완료 => 화면에 선택된 카테고리의 상품 목록 출력.
-			//productList.innerHTML = "";
-			productList.querySelectorAll('.col-md-6').forEach((c, idx) => {
-				if (idx != 0) {
-					c.remove();
-				}
-			});
-			console.log(this.response);
-			let result = JSON.parse(this.response);
-			result.forEach(product => {
-				productList.appendChild(makeRow(product));
-			});
-		});
-	});
-}*/
-
-document.querySelectorAll('.fas').forEach((d) => {
+categorySelectList.forEach((d) => {
 	d.addEventListener('click', function() {
-		let categoryName = d.innerHTML;
+		let target = d.getAttribute('data-bs-target');
+		let cateLocation = d.parentElement.parentElement.querySelector(target);
+		
+		let categoryName = d.querySelector('span').innerText;
 		csvc.categorySelectList(categoryName, function() {
 			// 상위카테고리 선택 완료 => 화면에 선택된 상위카테고리의 세부카테고리 출력.
 			//categoryList.innerHTML = "";
-			categorySelectList.querySelectorAll('.collapse').forEach((c, idx) => {
+			cateLocation.querySelectorAll('div').forEach((c, idx) => {
 				if (idx != 0) {
 					c.remove();
 				}
@@ -51,17 +36,45 @@ document.querySelectorAll('.fas').forEach((d) => {
 			console.log(this.response);
 			let result = JSON.parse(this.response);
 			result.forEach(category => {
-				categorySelectList.appendChild(makeRow(category));
+				cateLocation.appendChild(cmakeRow(category));
 			});
 		});
 	});
 
 });
 
-function makeRow(category = {}) {
-	let cloned = document.querySelector('#collapse>c>div').cloneNode(true);
+function cmakeRow(category = {}) {
+	let cloned = document.querySelector('.cloneapse>div').cloneNode(true);
 	cloned.setAttribute('data-category-no', category.categoryNo);
 	cloned.style.display = 'block';
-	cloned.querySelector('#collapse>c>div').innerText = category.categoryName;
+	cloned.innerText = category.cdName;
+	cloned.addEventListener('click', function() {
+		csvc.productList(category.cdName, function() {
+			// 카테고리 선택 완료 => 화면에 선택된 카테고리의 상품 목록 출력.
+			//productList.innerHTML = "";
+			productList.querySelectorAll('.col-md-6').forEach((c, idx) => {
+				if (idx != 0) {
+					c.remove();
+				}
+			});
+			//console.log(this.response);
+			let result = JSON.parse(this.response);
+			result.forEach(product => {
+				productList.appendChild(makeRow(product));
+			});
+		});
+	});
+	return cloned;
+}
+
+
+function makeRow(product = {}) {
+	let cloned = document.querySelector('#productList>div').cloneNode(true);
+	cloned.setAttribute('data-product-no', product.productNo);
+	cloned.style.display = 'block';
+	cloned.querySelector('.col-md-6>div>div:nth-of-type(2)').innerText = product.cdName;
+	cloned.querySelector('.col-md-6>div>div:nth-of-type(3)>h4').innerText = product.productName;
+	cloned.querySelector('.col-md-6>div>div:nth-of-type(3)>p').innerText = product.productContent;
+	cloned.querySelector('.col-md-6>div>div:nth-of-type(3)>div>p').innerText = product.productPrice+"원";
 	return cloned;
 }
